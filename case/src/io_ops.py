a3d3a214493b56244a41fab2f6a65fe827344b8c
Allan L. R. Hansen <allan.lrh@gmail.com>
2022-01-15 23:52:24 +0100

    Add a proper datetime column to the dataframe

--- case/src/io_ops.py
+++ case/src/io_ops.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from dateutil.parser import parse as dtparse
 import pathlib
 
 
@@ -6,4 +7,8 @@ def load_sf_csv(path: pathlib.Path):
     if not path:
         raise FileNotFoundError(f"Can't find the data file {path.absolute()}")
     df = pd.read_csv(path, sep=";", index_col="id")
+    df["datetime"] = pd.to_datetime(
+        df.date.astype(str) + " " + df.time.astype(str), format="%m/%d/%Y %H:%M"
+    )
+    df = df.drop(["date", "time"], axis=1)
     return df
