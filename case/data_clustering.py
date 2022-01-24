2924d7d0cd15f2d8b825d5fc6c17467f12fa84a3
Allan L. R. Hansen <allan.lrh@gmail.com>
2022-01-24 01:19:24 +0100

    Cluster incestigation done

--- case/data_clustering.py
+++ case/data_clustering.py
@@ -2,7 +2,6 @@
 # -*- coding: utf8 -*-
 
 # %% Imports and constants
-import datetime
 
 import hdbscan
 import matplotlib as mpl
@@ -12,64 +11,73 @@ import pandas as pd
 import seaborn as sns
 from sklearn import cluster
 
-from src.io_ops import load_sf_dataset
+from src.io_ops import load_sf_dataset, save_figure
 from src.preprocess_for_ml import (
     preprocess_for_clustering,
     preprocess_for_clustering_coordinates_only,
 )
-from src.shared_ressources import case_root
+from src.shared_ressources import case_root, weekdays, ScikitModel
 from src.shared_ressources import streamlit_keplergl_config as config
-from src.shared_ressources import weekdays
 
-# %%
+# %% supporting functions
+def plot_cluster(df: pd.DataFrame) -> tuple[plt.Figure, plt.Axes]:
+    fig, ax = plt.subplots(figsize=(9, 9))
+    sns.scatterplot(
+        data=df,
+        x="longitude",
+        y="latitude",
+        hue="clst",
+        ax=ax,
+    )
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    return fig, ax
+
+
+# %%Load the data, subsample for fast experimentation
 df = load_sf_dataset()
-# clusterer = hdbscan.HDBSCAN(
-#     min_cluster_size=4, gen_min_span_tree=True, cluster_selection_epsilon=0.2
-# )
 
-# clusterer = cluster.AffinityPropagation(max_iter=200)
-# clusterer = cluster.KMeans(4)
-
-sub = df.sample(2500)
-X = preprocess_for_clustering(sub).drop(["longitude", "latitude"], axis=1)
+sub = df.sample(5000)
 # X = preprocess_for_clustering_coordinates_only(sub).to_numpy()
-# %%
-clusterer = cluster.AgglomerativeClustering(
-    linkage="complete", affinity="manhattan", n_clusters=5
+# %% Check for structure WITHOUT the coordinates... does crime label and timestamp correlate with an area? Spoiler: nope!
+X = preprocess_for_clustering(sub).drop(["longitude", "latitude"], axis=1)
+clf = cluster.AgglomerativeClustering(
+    # manhattan distance becahse that's the relevant distance with this city planning
+    linkage="complete",
+    affinity="manhattan",
+    n_clusters=5,
 )
-clst = clusterer.fit_predict(X)
+clst = clf.fit_predict(X)
 sub["clst"] = clst
-set(clst)
+fig, ax = plot_cluster(sub)
+save_figure(fig, "agglomrative_clustering_without_coordinates_visualize_coordinates")
 
-
-# # %%
-fig, ax = plt.subplots(figsize=(9, 9))
-sns.scatterplot(
-    data=sub,
-    x="longitude",
-    y="latitude",
-    hue="clst",
-    ax=ax,
-    # palette=mpl.colormaps.get("Set3"),
+# %% Make a version of X which includes the geographical information
+X = preprocess_for_clustering(sub)
+# %%  Use a density baserd clustering algorithm
+clf = hdbscan.HDBSCAN(
+    min_cluster_size=50, gen_min_span_tree=True, cluster_selection_epsilon=0.2
 )
-
+clst = clf.fit_predict(X)
+sub["clst"] = clst
+fig, ax = plot_cluster(sub)
 
 # %%
-# %%
-# colors = np.array(list("rgbmcyk"))
+for n_clusters in range(3, 8):
+    clf = cluster.KMeans(n_clusters=n_clusters)
+    clst = clf.fit_predict(X)
+    sub["clst"] = clst
+    fig, ax = plot_cluster(sub)
+    save_figure(fig, f"kmeans_k_{n_clusters}_visualize_coordinates")
 
-color_palette = sns.color_palette("deep", 8)
-cluster_colors = [
-    color_palette[x] if x >= 0 else (0.5, 0.5, 0.5) for x in clusterer.labels_
-]
-cluster_member_colors = [
-    sns.desaturate(x, p) for x, p in zip(cluster_colors, clusterer.probabilities_)
-]
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.scatter(
-    *sub[["longitude", "latitude"]].to_numpy().T,
-    s=10,
-    linewidth=0,
-    c=cluster_member_colors,
-    alpha=0.75
-)
+# %%
+for n_clusters in range(3, 8):
+    clf = cluster.AgglomerativeClustering(
+        linkage="complete", affinity="manhattan", n_clusters=n_clusters
+    )
+    clst = clf.fit_predict(X)
+    sub["clst"] = clst
+    fig, ax = plot_cluster(sub)
+    save_figure(
+        fig, f"agglomerative_clustering_{n_clusters}_clusters_visualize_coordinates"
+    )
